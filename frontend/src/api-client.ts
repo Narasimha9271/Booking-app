7fc4c5e4e0d2e2499cc0fe9bd8f3ff5b94839380
Narasimha <Narasimhapalla729@gmail.com>
2024-01-28 18:48:56 +0530

    Fix return type in fetchMyHotelById function

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -87,7 +87,7 @@ export const fetchMyHotels = async (): Promise<HotelType[]> => {
   return response.json();
 };
 
-export const fetchMyHotelById = async (hotelId: string): Promise<HotelType[]> => {
+export const fetchMyHotelById = async (hotelId: string): Promise<HotelType> => {
   const response = await fetch(`${API_BASE_URL}/api/my-hotels/${hotelId}`, {
     credentials: "include"
   });
