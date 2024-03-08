1c5b879fec59f60d3d420822c4c5da5226106b1f
Narasimha <Narasimhapalla729@gmail.com>
2024-03-08 21:34:47 +0530

    db updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./db";
+// import connectDB from "./db";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import myHotelRoutes from "./routes/my-hotels";
@@ -10,14 +10,14 @@ import bookingRoutes from "./routes/my-bookings";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-// mongoose.connect(process.env.MONGODB_CONNECTION_STRING  as string)
+mongoose.connect(process.env.MONGODB_URI as string);
 
 const app = express();
 app.use(cookieParser());
@@ -38,17 +38,25 @@ app.use("/api/my-hotels", myHotelRoutes);
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/my-bookings", bookingRoutes);
 
-connectDB()
-    .then(() => {
-        app.on("error", (err) => {
-            console.log(err);
-            throw err;
-        });
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
-        app.listen(process.env.PORT || 7000, () => {
-            console.log(`Server is running at port: ${process.env.PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log("MONGO DB connection failed!!!", err);
-    });
+app.listen(7000, () => {
+    console.log("server running on localhost:7000");
+});
+
+// connectDB()
+//     .then(() => {
+//         app.on("error", (err) => {
+//             console.log(err);
+//             throw err;
+//         });
+
+//         app.listen(process.env.PORT || 7000, () => {
+//             console.log(`Server is running at port: ${process.env.PORT}`);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log("MONGO DB connection failed!!!", err);
+//     });
