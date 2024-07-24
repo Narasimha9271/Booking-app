d48994d6c3cc335bb4f579ed269629e59d821759
Narasimha <Narasimhapalla729@gmail.com>
2024-07-24 15:42:56 +0530

    lazy load && suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,112 +4,116 @@ import {
     Routes,
     Navigate,
 } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Layout from "./layouts/Layout";
-import Register from "./pages/Register";
-import SignIn from "./pages/SignIn";
-import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
-import MyHotels from "./pages/MyHotels";
-import EditHotel from "./pages/EditHotel";
-import Search from "./pages/Search";
-import Detail from "./pages/Detail";
-import Booking from "./pages/Booking";
-import Home from "./pages/Home";
-import MyBookings from "./pages/MyBookings";
+
+const Register = lazy(() => import("./pages/Register"));
+const SignIn = lazy(() => import("./pages/SignIn"));
+const AddHotel = lazy(() => import("./pages/AddHotel"));
+const MyHotels = lazy(() => import("./pages/MyHotels"));
+const EditHotel = lazy(() => import("./pages/EditHotel"));
+const Search = lazy(() => import("./pages/Search"));
+const Detail = lazy(() => import("./pages/Detail"));
+const Booking = lazy(() => import("./pages/Booking"));
+const Home = lazy(() => import("./pages/Home"));
+const MyBookings = lazy(() => import("./pages/MyBookings"));
 
 const App = () => {
     const { isLoggedIn } = useAppContext();
     return (
         <Router>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <Layout>
-                            <Home />
-                        </Layout>
-                    }
-                />
-                <Route
-                    path="/search"
-                    element={
-                        <Layout>
-                            <Search />
-                        </Layout>
-                    }
-                />
-                <Route
-                    path="/detail/:hotelId"
-                    element={
-                        <Layout>
-                            <Detail />
-                        </Layout>
-                    }
-                />
-                <Route
-                    path="/register"
-                    element={
-                        <Layout>
-                            <Register />
-                        </Layout>
-                    }
-                />
-                <Route
-                    path="/sign-in"
-                    element={
-                        <Layout>
-                            <SignIn />
-                        </Layout>
-                    }
-                />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Layout>
+                                <Home />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/search"
+                        element={
+                            <Layout>
+                                <Search />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/detail/:hotelId"
+                        element={
+                            <Layout>
+                                <Detail />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/register"
+                        element={
+                            <Layout>
+                                <Register />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/sign-in"
+                        element={
+                            <Layout>
+                                <SignIn />
+                            </Layout>
+                        }
+                    />
 
-                {isLoggedIn && (
-                    <>
-                        <Route
-                            path="/hotel/:hotelId/booking"
-                            element={
-                                <Layout>
-                                    <Booking />
-                                </Layout>
-                            }
-                        />
+                    {isLoggedIn && (
+                        <>
+                            <Route
+                                path="/hotel/:hotelId/booking"
+                                element={
+                                    <Layout>
+                                        <Booking />
+                                    </Layout>
+                                }
+                            />
 
-                        <Route
-                            path="/add-hotel"
-                            element={
-                                <Layout>
-                                    <AddHotel />
-                                </Layout>
-                            }
-                        />
-                        <Route
-                            path="/edit-hotel/:hotelId"
-                            element={
-                                <Layout>
-                                    <EditHotel />
-                                </Layout>
-                            }
-                        />
-                        <Route
-                            path="/my-hotels"
-                            element={
-                                <Layout>
-                                    <MyHotels />
-                                </Layout>
-                            }
-                        />
-                        <Route
-                            path="/my-bookings"
-                            element={
-                                <Layout>
-                                    <MyBookings />
-                                </Layout>
-                            }
-                        />
-                    </>
-                )}
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
+                            <Route
+                                path="/add-hotel"
+                                element={
+                                    <Layout>
+                                        <AddHotel />
+                                    </Layout>
+                                }
+                            />
+                            <Route
+                                path="/edit-hotel/:hotelId"
+                                element={
+                                    <Layout>
+                                        <EditHotel />
+                                    </Layout>
+                                }
+                            />
+                            <Route
+                                path="/my-hotels"
+                                element={
+                                    <Layout>
+                                        <MyHotels />
+                                    </Layout>
+                                }
+                            />
+                            <Route
+                                path="/my-bookings"
+                                element={
+                                    <Layout>
+                                        <MyBookings />
+                                    </Layout>
+                                }
+                            />
+                        </>
+                    )}
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </Suspense>
         </Router>
     );
 };
